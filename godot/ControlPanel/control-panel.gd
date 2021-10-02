3ae5bcdc3feff6fb8665080f67a51923a09571fe
Toni Schindler <toni.schindler@toschiwa.de>
2021-10-02 20:01:49 +0200

    Small bug fixes

--- godot/ControlPanel/control-panel.gd
+++ godot/ControlPanel/control-panel.gd
@@ -4,11 +4,12 @@ onready var subtitles_label = get_node("MarginContainer/SubtitlesLabel")
 onready var json_file_parser = get_node("JsonFileParser")
 onready var audio_stream_player = get_node("AudioStreamPlayer")
 
-export(String) var filename_subtitles
+export(String) var filename_subtitles = "example-audio"
 
 func _ready():
 	var filepath_subtitles = "res://Recordings/" + filename_subtitles + ".json"
 	var subtitles = json_file_parser.parse_file(filepath_subtitles)
 	subtitles_label.map_subtitles_to_animation(subtitles)
+	subtitles_label.start_subtitles()
 	
 	
--- godot/ControlPanel/subtitles-label.gd
+++ godot/ControlPanel/subtitles-label.gd
@@ -8,28 +8,30 @@ var method_track_index : int = 0
 onready var animation_player = get_node("AnimationPlayer")
 
 func _ready():
-	animation_player.connect("text_started", "_on_text_started")
-	animation_player.connect("text_ended", "_on_text_ended")
+	animation_player.connect("text_started", self, "_on_text_started")
+	animation_player.connect("text_ended", self, "_on_text_ended")
 
 func map_subtitles_to_animation(subtitles_json):
 	subtitles_dict = subtitles_json
 
 	var duration = _subtitles_duration()
 	current_animation = Animation.new()
-	animation_player.add_animation(animation_name, current_animation)
-	animation_player.set_current_animation(animation_name)
 	
 	method_track_index = current_animation.add_track(Animation.TYPE_METHOD)
+	current_animation.track_set_path(method_track_index, animation_player.get_path())
 
-	animation_player.get_animation(animation_name).length = duration
+	current_animation.length = duration
 	
 	for interval in subtitles_dict:
-		assert("begin" in interval)
+		assert("start" in interval)
 		assert("end" in interval)
 		assert("text" in interval)
 		
-		_set_text_start_animation(interval["begin"], interval["text"])
-		_set_text_end_animation(interval["end"], interval["text"])
+		_set_text_start_animation(float(interval["start"]), interval["text"])
+		_set_text_end_animation(float(interval["end"]), interval["text"])
+
+	animation_player.add_animation(animation_name, current_animation)
+	animation_player.set_current_animation(animation_name)
 
 func start_subtitles():
 	animation_player.play()
@@ -40,16 +42,16 @@ func _subtitles_duration():
 
 	var last_interval = subtitles_dict[subtitles_dict.size()-1]
 	assert("end" in last_interval)
-	return last_interval["end"]
+	return float(last_interval["end"])
 
-func _set_text_start_animation(time, text):
-	animation_player.track_insert_key(method_track_index, time, 
+func _set_text_start_animation(time : float, text : String):
+	current_animation.track_insert_key(method_track_index, time, 
 									{	"method" : "emit_signal" , 
 										"args" : ["text_started", text]
 									})
 
-func _set_text_end_animation(time, text):
-	animation_player.track_insert_key(method_track_index, time, 
+func _set_text_end_animation(time : float, text : String):
+	current_animation.track_insert_key(method_track_index, time, 
 									{	"method" : "emit_signal" , 
 										"args" : ["text_ended", text]
 									})
