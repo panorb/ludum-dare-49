678cf05f3eb57f27fc11c23bd9d4ac53bf665920
Toni Schindler <toni.schindler@toschiwa.de>
2021-10-02 22:32:12 +0200

    Minor changes in file names

--- godot/ControlPanel/control-panel.gd
+++ godot/ControlPanel/control-panel.gd
@@ -1,6 +1,6 @@
 extends Control
 
-onready var subtitles_label = get_node("MarginContainer/SubtitlesLabel")
+onready var subtitle_label = get_node("MarginContainer/SubtitleLabel")
 onready var json_file_parser = get_node("JsonFileParser")
 onready var audio_stream_player = get_node("AudioStreamPlayer")
 
@@ -10,9 +10,10 @@ export(String) var filename_recording
 func _ready():
 	var filepath_subtitles = "res://Recordings/" + filename_subtitles + ".json"
 	var subtitles = json_file_parser.parse_file(filepath_subtitles)
-	subtitles_label.map_subtitles_to_animation(subtitles)
-	subtitles_label.start_subtitles()
+	subtitle_label.map_subtitles_to_animation(subtitles)
 	
-	var filepath_recording = "res://Recordings/" + filename_recording + ".flac"
+	var filepath_recording = "res://Recordings/" + filename_recording + ".wav"
 	audio_stream_player.stream = load(filepath_recording)
+	
+	subtitle_label.start_subtitles()
 	audio_stream_player.play()
