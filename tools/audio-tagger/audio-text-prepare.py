1f8ac422a254ca1c9d277ac5ff789bb79656633e
Paul Norberger <panor@fastmail.com>
2021-10-02 17:40:25 +0200

    Added python script to prepare full text for youtube upload

---
+++ tools/audio-tagger/audio-text-prepare.py
@@ -0,0 +1,20 @@
+
+with open("russian-officer-full-text.txt", "r") as f:
+    text = f.read()
+
+# Remove all new lines
+text = text.replace("\n", " ")
+output_text = ""
+words = text.split(" ")
+
+
+for i in range(0, len(words), 2):
+    output_text += words[i]
+
+    if len(words) > i+1:
+        output_text += " " + words[i+1]
+
+    output_text += "\n\n"
+
+with open("output.txt", "w+") as f:
+    f.write(output_text)
