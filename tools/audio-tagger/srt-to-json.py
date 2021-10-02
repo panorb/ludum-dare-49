8a32904ee2df810963d51b7d6ab906c673fcd37f
Paul Norberger <panor@fastmail.com>
2021-10-02 21:38:36 +0200

    Updated srt to json script to convert timestamps to seconds

--- tools/audio-tagger/srt-to-json.py
+++ tools/audio-tagger/srt-to-json.py
@@ -1,4 +1,43 @@
 import json
+from datetime import timedelta
+import re
+
+RGX_TIMESTAMP_MAGNITUDE_DELIM = r"[,.:，．。：]"
+RGX_TIMESTAMP_FIELD = r"[0-9]+"
+RGX_TIMESTAMP_FIELD_OPTIONAL = r"[0-9]*"
+RGX_TIMESTAMP = "".join(
+    [
+        RGX_TIMESTAMP_MAGNITUDE_DELIM.join([RGX_TIMESTAMP_FIELD] * 3),
+        RGX_TIMESTAMP_MAGNITUDE_DELIM,
+        "?",
+        RGX_TIMESTAMP_FIELD_OPTIONAL,
+    ]
+)
+RGX_TIMESTAMP_PARSEABLE = r"^{}$".format(
+    "".join(
+        [
+            RGX_TIMESTAMP_MAGNITUDE_DELIM.join(
+                ["(" + RGX_TIMESTAMP_FIELD + ")"] * 3),
+            RGX_TIMESTAMP_MAGNITUDE_DELIM,
+            "?",
+            "(",
+            RGX_TIMESTAMP_FIELD_OPTIONAL,
+            ")",
+        ]
+    )
+)
+
+TS_REGEX = re.compile(RGX_TIMESTAMP_PARSEABLE)
+
+
+def srt_timestamp_to_timedelta(timestamp):
+    match = TS_REGEX.match(timestamp)
+    if match is None:
+        raise TimestampParseError(
+            "Unparseable timestamp: {}".format(timestamp))
+    hrs, mins, secs, msecs = [int(m) if m else 0 for m in match.groups()]
+    return timedelta(hours=hrs, minutes=mins, seconds=secs, milliseconds=msecs)
+
 
 with open("input.srt", "r") as f:
     srt_lines = f.readlines()
@@ -14,8 +53,8 @@ for i in range(0, len(srt_lines), 4):
 
     list_.append({
         "index": index,
-        "start": start_time,
-        "end": end_time,
+        "start": srt_timestamp_to_timedelta(start_time).total_seconds(),
+        "end": srt_timestamp_to_timedelta(end_time).total_seconds(),
         "text": text
     })
 
