04e9dd2f99be21efb870ad9c5656e85d93a82404
Toni Schindler <toni.schindler@toschiwa.de>
2021-10-04 01:21:00 +0200

    Update censored intervals in subtitle container

--- godot/ControlPanel/subtitle-label.gd
+++ godot/ControlPanel/subtitle-label.gd
@@ -9,10 +9,8 @@ var current_animation : Animation = null
 var method_track_index_start : int = 0
 var method_track_index_end : int = 1
 var current_segment_id: int = -1
-var current_index : int = -1
+var current_index : int = 0
 var preset_formatting : Dictionary = {}
-var censor_button_pressed : bool = false setget _toggle_censor_button
-var current_line_formatted = ""
 var last_cursor_position : int = 0 
 
 onready var animation_player = get_node("AnimationPlayer")
@@ -47,6 +45,14 @@ func map_subtitles_to_animation(subtitles_json):
 		_set_text_end_animation(index)
 
 	animation_player.add_animation(animation_name, current_animation)
+
+func update_censored_intervals(index : int, censored_intervals : Array):
+	if not subtitles_dict:
+		return
+	if index >= subtitles_dict.size():
+		return
+	
+	subtitles_dict[index]["censored_intervals"] = censored_intervals
 
 func start_subtitles():
 	animation_player.set_current_animation(animation_name)
@@ -91,15 +97,11 @@ func _on_text_started(index : int):
 	current_index = index
 	if current_segment_id != subtitles_dict[index]["segment"]:
 		current_segment_id = subtitles_dict[index]["segment"]
-		current_line_formatted = ""
-		if censor_button_pressed:
-			current_line_formatted += "[color=#000F00]"
 		_create_upcoming_lines()
 	last_cursor_position = 0
 	emit_signal("text_started", index)
 
 func _on_text_ended(index : int):
-	current_line_formatted = current_line_formatted + " "
 	emit_signal("text_ended", index)
 
 func _create_upcoming_lines():
@@ -120,16 +122,20 @@ func _create_upcoming_lines():
 				subtitles_dict[index]["segment"] == current_segment_id + line_index:
 				
 				var text_element = subtitles_dict[index]["text"]
+				var formatted_text_element = text_element
 				
-				if index in preset_formatting.keys():
-					for property in preset_formatting[index].keys():
-						match property:
-							"color":
-								text_element = "[color=" \
-												+ preset_formatting[index][property]
-												
-					
-				line = PoolStringArray([line, subtitles_dict[index]["text"]]).join(" ")
+				if "censored_intervals" in subtitles_dict[index].keys():
+					var left = 0
+					var right = 0
+					for interval in subtitles_dict[index]["censored_intervals"]:
+						left = interval["start_position"]
+						formatted_text_element += text_element.substr(right, left - right)
+						right = interval["end_position"]
+						formatted_text_element += "[color=#000F00]" + text_element.substr(left, right-left) \
+													+ "[/color]"
+						formatted_text_element += text_element.substr(right)
+				
+				line = PoolStringArray([line, formatted_text_element]).join(" ")
 				index += 1
 				
 		lines[line_index].bbcode_text = line
@@ -143,39 +149,71 @@ func _render_text():
 	var text_length = subtitles_dict[current_index]["text"].length()
 	var new_cursor_position = round(text_length * share)
 	
-	if new_cursor_position != last_cursor_position or new_cursor_position == 0:
-		lines[0].bbcode_text = _update_current_line(new_cursor_position)
+	if new_cursor_position != last_cursor_position:
+		last_cursor_position = new_cursor_position
+		_update_current_line()
 
-func _update_current_line(new_cursor_position : int):
+func _update_current_line():
 	var line = ""
-	var index = current_index - 1
+	var index = max(current_index - 1, 0)
 	
-	var extension = subtitles_dict[current_index]["text"].substr(last_cursor_position,
-			new_cursor_position - last_cursor_position)
+	# Find first text elment of segment
+	while index >= 0 and subtitles_dict[index]["segment"] == current_segment_id:
+		index -= 1
 	
-	current_line_formatted = current_line_formatted + extension
-	
-	line = current_line_formatted
-	
-	if line.length() > 0:
-		if censor_button_pressed:
-			line += "[/color]"
-		line = "[color=#FF0000]" + line + "[/color]"
-	
-	line += subtitles_dict[current_index]["text"].substr(new_cursor_position)
-	
-	# Part after the current text
-	index = current_index + 1
-	while index < subtitles_dict.size() and subtitles_dict[index]["segment"] == current_segment_id:
-		line += " " + subtitles_dict[index]["text"]
+	if index < 0 or subtitles_dict[index]["segment"] != current_segment_id:
 		index += 1
 	
-	last_cursor_position = new_cursor_position
-	return line
+	if subtitles_dict[index]["segment"] != current_segment_id:
+		return
+	
+	while subtitles_dict[index]["segment"] == current_segment_id:
+		var text_element = subtitles_dict[index]["text"]
+		var formatted_text_element = ""
 
-func _toggle_censor_button(value : bool) -> void:
-	censor_button_pressed = value
-	if censor_button_pressed:
-		current_line_formatted += "[color=#000F00]"
-	else:
-		current_line_formatted += "[/color]"
+		var left = 0
+		var right = 0
+		
+		if "censored_intervals" in subtitles_dict[index].keys():
+			for interval in subtitles_dict[index]["censored_intervals"]:
+				left = interval["start_position"]
+				if index == current_index:
+					# Cursor position passed
+					if left >= last_cursor_position and right <= last_cursor_position:
+						formatted_text_element  += text_element.substr(right, last_cursor_position - right)
+						formatted_text_element += "[/color]"
+						formatted_text_element += text_element.substr(last_cursor_position, left-last_cursor_position)
+					else:
+						formatted_text_element += text_element.substr(right, left - right)
+
+					# If no end position key is present, the text is censored until the current cursor position
+					if not "end_position" in interval.keys():
+						right = last_cursor_position
+					else:
+						right = interval["end_position"]
+		
+					# Cursor is inside the current censored interval
+					if left < last_cursor_position and right >= last_cursor_position:
+						# Split the censor color tags to insert the closing cursor color tag
+						formatted_text_element += "[color=#000F00]" + text_element.substr(left, last_cursor_position-left) + "[/color]"
+						formatted_text_element += "[/color]"
+						formatted_text_element += "[color=#000F00]" + text_element.substr(last_cursor_position, right-last_cursor_position) + "[/color]"
+				else:
+					formatted_text_element += text_element.substr(right, left - right)
+					right = interval["end_position"]
+					formatted_text_element += "[color=#000F00]" + text_element.substr(left, right-left) \
+											+ "[/color]"
+			
+		if index == current_index and (right < last_cursor_position):
+			formatted_text_element += text_element.substr(right, last_cursor_position-right)
+			formatted_text_element += "[/color]"
+			formatted_text_element += text_element.substr(last_cursor_position)
+		else:
+			formatted_text_element += text_element.substr(right)
+
+		line = PoolStringArray([line, formatted_text_element]).join(" ")
+		index += 1
+	
+	print(line)
+	lines[0].bbcode_text = "[color=#FF0000]" + line
+
