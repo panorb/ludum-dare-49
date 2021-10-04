88483e76378e3a01f217747200cd38eb0eba814a
Paul Norberger <panor@fastmail.com>
2021-10-04 21:33:00 +0200

    Stuff

--- godot/Game/game.gd
+++ godot/Game/game.gd
@@ -6,8 +6,8 @@ onready var censor_button = get_node("CensorButton")
 onready var energy_meter = get_node("EnergyMeter")
 onready var evaluation = get_node("Evaluation")
 
-export(int) var energy_regeneration_multiplier = 520
-export(int) var energy_depletion_multiplier = 140
+export(int) var energy_regeneration_multiplier = 170
+export(int) var energy_depletion_multiplier = 520
 
 var last_ended_chapter := ""
 var censor_button_down = false
@@ -15,28 +15,41 @@ var censor_button_down = false
 func _ready():
 	playback.load_character("russian-officer")
 	censor_button.enabled = false
-	_start_chapter("example")
+	_start_chapter("beginning")
 	
-	playback.connect("chapter_ended", self, "_on_chapter_ended")
+	playback.connect("chapter_ended", self, "_on_chapter_playback_ended")
+	evaluation.connect("evaluation_finished", self, "_on_chapter_evaluation_finished")
 
 func _process(delta):
 	if censor_button_down:
-		energy_meter.value -= energy_regeneration_multiplier * delta
+		energy_meter.value -= energy_depletion_multiplier * delta
 		
 		if energy_meter.value == 0:
 			playback.stop_censoring()
 	else:
-		energy_meter.value += energy_depletion_multiplier * delta
+		energy_meter.value +=  energy_regeneration_multiplier * delta
 		
 		
 
 var censorship_allowed = ["example", "segment-1", "segment-2", "segment-3"]
 
 func _start_chapter(chapter_id):
+	evaluation.visible = false
+	playback.visible = true
+	
 	censor_button.enabled = chapter_id in censorship_allowed
 	playback.play_chapter(chapter_id)
 
-func _on_chapter_ended(chapter_id : String, saved_data : Dictionary) -> void:
+func _start_evaluation(chapter_id : String, saved_data : Dictionary, character_name : String) -> void:
+	playback.visible = false
+	evaluation.visible = true
+	
+	censor_button.enabled = false
+	
+	evaluation.initialize(saved_data, character_name)
+	evaluation.play_chapter(chapter_id)
+
+func _on_chapter_playback_ended(chapter_id : String, saved_data : Dictionary) -> void:
 	last_ended_chapter = chapter_id
 	
 	# Russischer Anfang
@@ -50,12 +63,14 @@ func _on_chapter_ended(chapter_id : String, saved_data : Dictionary) -> void:
 		_start_chapter("example")
 	
 	if chapter_id == "example":
-		# TODO: Auswertung hier einfügen
-		playback.hide()
-		evaluation.initialize(saved_data, "russian-officer")
-		evaluation.play_chapter(chapter_id)
+		_start_evaluation("example", saved_data, "russian-officer")
+		
+#		# TODO: Auswertung hier einfügen
+#		playback.hide()
+#		evaluation.initialize(saved_data, "russian-officer")
+#		evaluation.play_chapter(chapter_id)
 		# TODO: Richtige Voicezeile (Success oder Failure) als Anmerkung des Officers auswählen
-		#_start_chapter("example-failure")
+		# _start_chapter("example-failure")
 	
 	if chapter_id == "example-failure":
 		repeat_choice.present()
@@ -63,7 +78,7 @@ func _on_chapter_ended(chapter_id : String, saved_data : Dictionary) -> void:
 	if chapter_id == "example-success":
 		# TODO: Übergang zum Amerikaner
 		playback.load_character("american-spy")
-		_start_chapter("segment-3")
+		_start_chapter("segment-1")
 	
 	# Amerikanisches Hauptspiel
 	if chapter_id == "segment-1":
@@ -78,6 +93,13 @@ func _on_chapter_ended(chapter_id : String, saved_data : Dictionary) -> void:
 		# TODO: Richtiges Ending je nach Penalityanzahl wählen
 		playback.load_character("russian-officer")
 		_start_chapter("ending-wtf")
+
+func _on_chapter_evaluation_finished(chapter_id : String, penalty_score : int):
+	if chapter_id == "example":
+		if penalty_score > 5:
+			_start_chapter("example-failure")
+		else:
+			_start_chapter("example-success")
 
 func _on_RepeatChoice_proceed_selected():
 	if last_ended_chapter in ["beginning", "instructions"]:
@@ -94,6 +116,7 @@ func _on_RepeatChoice_repeat_selected():
 		_start_chapter("repeat")
 	
 	if last_ended_chapter == "example-failure":
+		playback.load_character("russian-officer")
 		_start_chapter("example-intro")
 
 
